fc822d0bc941a0914e4206d256c64a4deed74cb3
unknown <gnitish31@gmail.com>
2021-01-22 19:36:40 +0530

    general commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8000;
 const mongoose = require("mongoose")
 
 //connecting with mongodb named todo
-mongoose.connect('mongodb+srv://nitish:nitish@cluster0.9uw8i.mongodb.net/todo?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, 'useFindAndModify': false, 'useCreateIndex': true })
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true, 'useFindAndModify': false, 'useCreateIndex': true })
     .then(() => console.log("connection succesfull"))
     .catch(() => console.log("error in connecting database"));
 
